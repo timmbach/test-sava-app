fcf6e1988bff935bc09bb6cf7955f8b77a788cdb
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2022-11-15 10:43:49 +0100

    sidebar updated

--- src/components/layout/Sidebar/Sidebar.jsx
+++ src/components/layout/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Sidebar.module.css";
 import Logo from "../../../assets/svg/logo.svg";
+import UserImage from "../../../assets/userAvatar.png"
 import { navData } from "./sidebarData";
 import { CaretIcon } from "../../../assets/svg";
 import { SubNav } from "./SubNav/SubNav";
@@ -14,10 +15,10 @@ export const Sidebar = () => {
 
 	return (
 		<div className={styles.sidebar_wrapper}>
-			<img src={Logo} alt="logo" className={styles.logo} />
+			<nav className={styles.nav_wrapper}>
+				<img src={Logo} alt="logo" className={styles.logo} />
 
-			<nav>
-				<ul className={styles.nav_wrapper}>
+				<ul className={styles.nav_list_wrapper}>
 					{navData.map(({ status, id, Icon, title, href }) => (
 						<li key={id} className={styles.nav_list_item}>
 							<a href={href} className={`${styles.nav_list} ${status === "active" ? styles.active : null}`}>
@@ -35,6 +36,20 @@ export const Sidebar = () => {
 					))}
 				</ul>
 			</nav>
+
+			<div className={styles.user_wrapper}>
+				<div className={styles.user_details}>
+					<span className={styles.user_img}>
+						<img src={UserImage} alt="" />
+					</span>
+					<p>Toni Davies</p>
+				</div>
+
+				<div className={styles.storage_details}>
+					<small className={styles.storage_text}>500mb of 5GB used</small>
+					<span className={styles.storage_space}></span>
+				</div>
+			</div>
 		</div>
 	);
 };
