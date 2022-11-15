91f6d1e80f2fc71b9c1dc1dba9279e10546bec10
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2022-11-15 12:52:40 +0100

    commented out auth

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,43 +1,43 @@
-import React, { useState } from "react";
-import { Alert, Button, Card } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
+import React, { useState } from 'react'
+import { Alert, Button, Card } from 'react-bootstrap'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../../contexts/AuthContext'
 
 const Dashboard = () => {
-  const [error, setError] = useState("");
-  const { currentUser, signout } = useAuth();
-  const history = useNavigate();
+  const [error, setError] = useState('')
+  // const { currentUser, signout } = useAuth();
+  const history = useNavigate()
 
   const handleSignOut = async function () {
-    setError("");
+    setError('')
 
     try {
-      await signout();
-      history("/signin");
+      // await signout();
+      history('/signin')
     } catch {
-      setError("Failed to sign out");
+      setError('Failed to sign out')
     }
-  };
+  }
 
   return (
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-3">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="w-100 btn btn-primary mt-3">
+          <h2 className='text-center mb-3'>Profile</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          {/* <strong>Email:</strong> {currentUser.email} */}
+          <Link to='/update-profile' className='w-100 btn btn-primary mt-3'>
             Update Profile
           </Link>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-4">
-        <Button variant="link" onClick={handleSignOut}>
+      <div className='w-100 text-center mt-4'>
+        <Button variant='link' onClick={handleSignOut}>
           Sign Out
         </Button>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
--- src/components/PrivateRoutes.js
+++ src/components/PrivateRoutes.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function PrivateRoutes() {
-  const { currentUser } = useAuth();
+  // const { currentUser } = useAuth()
 
-  return currentUser ? <Outlet /> : <Navigate to="/landing-page" />;
+  // return currentUser ? <Outlet /> : <Navigate to='/landing-page' />
+
+  return <Outlet />
 }
