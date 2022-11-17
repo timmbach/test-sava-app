01f319ae021180606a7a2b47fb2e3275702fadcb
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-11-17 03:20:44 +0100

    Add routes to categories page and dynamic category item page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,21 @@
-
-import React from "react";
+import React from 'react';
 // import { Container } from "react-bootstrap";
 // import { AuthProvider } from "../contexts/AuthContext";
 // import { AuthProvider } from "../../contexts/AuthContext";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "../layout/Layout";
-import SignUp from "../SignUp/SignUp";
-import SignIn from "../SignIn/SignIn";
-import PrivateRoutes from "../PrivateRoutes";
-import ForgotPassword from "../ForgotPassword/ForgotPassword";
-import UpdateProfile from "../UpdateProfile/UpdateProfile";
-import LandingPage from "../LandingPage/LandingPage";
-import TermsAndConditions from "../TermsAndConditions/TermsAndConditions";
-import NewPassword from "../NewPassword/NewPassword";
-import NoProfile from '../layout/No Profile/NoProfile'
-import Upload from '../Upload/Upload'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from '../layout/Layout';
+import SignUp from '../SignUp/SignUp';
+import SignIn from '../SignIn/SignIn';
+import PrivateRoutes from '../PrivateRoutes';
+import ForgotPassword from '../ForgotPassword/ForgotPassword';
+import UpdateProfile from '../UpdateProfile/UpdateProfile';
+import LandingPage from '../LandingPage/LandingPage';
+import TermsAndConditions from '../TermsAndConditions/TermsAndConditions';
+import NewPassword from '../NewPassword/NewPassword';
+import NoProfile from '../layout/No Profile/NoProfile';
+import Upload from '../Upload/Upload';
+import Categories from '../Categories/Categories';
+import Category from '../Category/Category';
 
 const App = () => {
   return (
@@ -24,24 +25,26 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route element={<PrivateRoutes />}>
-              <Route path='/' element={<Layout />}>
+              <Route path="/" element={<Layout />}>
                 <Route index element={<Upload />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="category/:name" element={<Category />} />
               </Route>
-              <Route path='/upload-new-user' element={<NoProfile />} />
-              <Route path='/update-profile' element={<UpdateProfile />} />
+              <Route path="/upload-new-user" element={<NoProfile />} />
+              <Route path="/update-profile" element={<UpdateProfile />} />
             </Route>
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/signin' element={<SignIn />} />
-            <Route path='/forgot-password' element={<ForgotPassword />} />
-            <Route path='/new-password' element={<NewPassword />} />
-            <Route path='/landing-page' element={<LandingPage />} />
-            <Route path='/terms-and-conditions' element={<TermsAndConditions />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/new-password" element={<NewPassword />} />
+            <Route path="/landing-page" element={<LandingPage />} />
+            <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
           </Routes>
         </BrowserRouter>
       </div>
     </div>
     // </AuthProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
