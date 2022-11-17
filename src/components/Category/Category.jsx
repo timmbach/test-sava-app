441452f2531086a03d603b9466f18f67766acb4e
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-11-17 03:49:33 +0100

    change  api to rick and morty to demonstrate dynamic api consumption

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -7,21 +7,34 @@ const Category = () => {
   const [pics, setPics] = useState([]);
   const params = useParams();
 
-  const fetchPictures = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/photos/?_limit=10');
-    const data = await response.json();
-    return data;
-  };
+  // This is just a mock. The comparison will be done on the backend,
+  // we just need to pass in the id of the category to reequest for such data.
+  let species = 'human';
+  switch (params.name) {
+    case 'you-and-pets':
+    case 'just-you':
+      species = 'human';
+      break;
+    default:
+      species = 'alien';
+      break;
+  }
 
   useEffect(() => {
+    const fetchPictures = async () => {
+      const response = await fetch(`https://rickandmortyapi.com/api/character/?species=${species}`);
+      const data = await response.json();
+      const refinedData = data.results.slice(0, 10);
+      return refinedData;
+    };
     const getImages = async () => {
       const fakePics = await fetchPictures();
       setPics((prev) => {
-        return [...prev, ...fakePics];
+        return [...fakePics];
       });
     };
     getImages();
-  }, []);
+  }, [species]);
 
   const formatCategoryName = () => {
     return params.name.split('-').join(' ');
@@ -32,8 +45,8 @@ const Category = () => {
       <h3>{formatCategoryName()}</h3>
       <div className={styles.images_grid}>
         {pics.length
-          ? pics.map(({ id, url, title }) => {
-              return <ImageInstance key={id} url={url} title={title} />;
+          ? pics.map(({ id, image, name }) => {
+              return <ImageInstance key={id} url={image} title={name} />;
             })
           : null}
       </div>
