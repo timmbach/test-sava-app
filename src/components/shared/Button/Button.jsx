48f7e895fca3cf95d29acc19e101d70b832dbecd
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2022-11-17 09:51:22 +0100

    upload functionality basic no styling yet

--- src/components/shared/Button/Button.jsx
+++ src/components/shared/Button/Button.jsx
@@ -4,10 +4,10 @@ import styles from './Button.module.css'
 export const Button = ({ text = 'Upload', manualStyle = styles.btn }) => {
   return (
     <div className={styles.button_shared}>
-      <label htmlFor='upload' className={manualStyle}>
+      <label htmlFor='server-upload' className={manualStyle}>
         {text}
       </label>
-      <input type='file' id='upload' accept='image/*' />
+      <input type='button' id='server-upload' />
     </div>
   )
 }
