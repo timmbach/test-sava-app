2db946b41230e0623345b8b3180fd5d54e39a287
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-11-17 03:25:04 +0100

    replace hypertext element with react router link component

--- src/components/layout/Sidebar/SubNav/SubNav.jsx
+++ src/components/layout/Sidebar/SubNav/SubNav.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import { categoriesData } from "../sidebarData";
-import styles from "./SubNav.module.css";
+import React from 'react';
+import { categoriesData } from '../sidebarData';
+import styles from './SubNav.module.css';
+import { Link } from 'react-router-dom';
 
 export const SubNav = () => {
-	return (
-		<ul className={styles.sub_nav_wrapper}>
-			{categoriesData.map((items) => (
-				<a href="/" key={items.id} className={styles.sub_nav_items}>
-					<li className={styles.sub_nav_title}>
-						<h6>{items.title}</h6>
-					</li>
-				</a>
-			))}
-		</ul>
-	);
+  return (
+    <ul className={styles.sub_nav_wrapper}>
+      {categoriesData.map((items) => (
+        <Link to={items.href} key={items.id} className={styles.sub_nav_items}>
+          <li className={styles.sub_nav_title}>
+            <h6>{items.title}</h6>
+          </li>
+        </Link>
+      ))}
+    </ul>
+  );
 };
